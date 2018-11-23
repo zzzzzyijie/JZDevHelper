ff1e6cf79435b638be00c5fac8cefeaf13dc8bc7
zengyijie <zengyijie@baibu.la>
2018-11-23 10:41:28 +0800

    test SafeArea

--- JZDevHelperDemo/JZDevHelperDemo/ViewController.m
+++ JZDevHelperDemo/JZDevHelperDemo/ViewController.m
@@ -32,16 +32,40 @@
         NSLog(@"DisBACK");
     });
     
-//    NSString *str = @"";
-//    if (str.isEmpty) {
-//        YJLog(@"空");
-//    }
+    NSString *str = @"";
+    if (str.isEmpty) {
+        YJLog(@"空 str");
+    }
+    
+    NSMutableArray *mArray = [NSMutableArray arrayWithArray:@[]];
+    if (mArray.isEmpty) {
+        YJLog(@"空 mArray ");
+    }
+    
+    NSMutableDictionary *mDict = [NSMutableDictionary dictionaryWithDictionary:@{}];
+    if (mDict.isEmpty) {
+        YJLog(@"空 mDict");
+    }
     
     YJLog(@"%@",[JZDeviceTool appBundleName]);
     YJLog(@"%@",[JZDeviceTool appBundleIdentifier]);
     YJLog(@"%@",[JZDeviceTool appVersion]);
     YJLog(@"%@",[JZDeviceTool appBuildVersion]);
     
+    YJLog(@"===========");
+    YJLog(@"self.view.frame=%@",NSStringFromCGRect(self.view.frame));
+}
+
+- (void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    
+    YJLog(@"viewDidLayoutSubviews - view.frame=%@",NSStringFromCGRect(self.view.frame));
+    
+    YJLog(@"self.view.safeAreaInsets=%@",NSStringFromUIEdgeInsets(self.view.safeAreaInsets));
+    // 如果self.navigationController.navigationBar.hidden = YES; -> 去掉导航栏高度 44
+    
+    // 8: self.view.safeAreaInsets={64, 0, 0, 0}
+    // X,XS,XR,MAX; self.view.safeAreaInsets={88, 0, 34, 0}
 }
 
 
@@ -52,16 +76,6 @@
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     
-    NSMutableArray *mArray = [NSMutableArray arrayWithArray:@[]];
-    
-    if (mArray.isEmpty) {
-        YJLog(@"空 mArray ");
-    }
-    
-    NSMutableDictionary *mDict = [NSMutableDictionary dictionaryWithDictionary:@{}];
-    if (mDict.isEmpty) {
-        YJLog(@"空 mDict");
-    }
 }
 
 
