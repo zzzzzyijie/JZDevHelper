c0a932504d9fa2556edaa46a68fc49d5d3609875
zengyijie <zengyijie@jianzhikeji.com>
2020-09-13 00:18:32 +0800

    Release 0.1.5

--- JZDevHelper/Category/UIKit/UITableView+JZAdd.h
+++ JZDevHelper/Category/UIKit/UITableView+JZAdd.h
@@ -9,6 +9,42 @@
 #import <UIKit/UIKit.h>
 
 @interface UITableView (JZAdd)
+
+#pragma mark - Cell
+/**
+注册nib
+*/
+- (void)jz_registerNib:(Class)className;
+
+/*
+ *  className为Xcell class，同时也为reuseIdentifier
+ */
+- (void)jz_registerClass:(Class )className;
+
+
+#pragma mark - Header & Footer
+/*
+ *  className为Xcell class，同时也为reuseIdentifier
+ */
+- (void)jz_registerHeaderFooterNib:(Class )className;
+
+/*
+ *  className为Xcell class，同时也为reuseIdentifier
+ */
+- (void)jz_registerHeaderFooterClass:(Class )className;
+
+/*
+ *  给给一个section设置圆角的显示
+ *  在tableView的willDisplayCell调用
+ */
+- (void)configCorner:(CGFloat)radius
+             padding:(CGFloat)padding
+           fillColor:(UIColor *)fillColor
+           tableView:(UITableView *)tableView
+                cell:(UITableViewCell *)cell
+   forRowAtIndexPath:(NSIndexPath *)indexPath;
+
 // 取消选择状态
 - (void)jz_clearsSelection;
+
 @end
--- JZDevHelper/Category/UIKit/UITableView+JZAdd.m
+++ JZDevHelper/Category/UIKit/UITableView+JZAdd.m
@@ -10,6 +10,57 @@
 
 @implementation UITableView (JZAdd)
 
+#pragma mark - Cell
+- (void)jz_registerClass:(Class )className {
+    [self registerClass:className forCellReuseIdentifier:NSStringFromClass(className)];
+}
+
+- (void)jz_registerNib:(Class)className{
+    NSString *name = NSStringFromClass(className);
+    [self registerNib:[UINib nibWithNibName:name bundle:nil] forCellReuseIdentifier:name];
+}
+
+#pragma mark - Header & Footer
+- (void)jz_registerHeaderFooterNib:(Class )className {
+    NSString *name = NSStringFromClass(className);
+    [self registerNib:[UINib nibWithNibName:name bundle:nil] forHeaderFooterViewReuseIdentifier:name];
+}
+
+- (void)jz_registerHeaderFooterClass:(Class )className {
+    [self registerClass:className forHeaderFooterViewReuseIdentifier:NSStringFromClass(className)];
+}
+
+
+- (void)configCorner:(CGFloat)radius
+             padding:(CGFloat)padding
+           fillColor:(UIColor *)fillColor
+           tableView:(UITableView *)tableView
+                cell:(UITableViewCell *)cell
+   forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    CGSize cornerRadius = CGSizeMake(radius, radius);
+    cell.backgroundColor = UIColor.clearColor;
+    CAShapeLayer *layer = [[CAShapeLayer alloc] init];
+    CGRect bounds = CGRectInset(cell.bounds, padding, 0);
+    
+    UIBezierPath *path;
+    if (indexPath.row == 0 && indexPath.row == [tableView numberOfRowsInSection:indexPath.section]-1) {
+       path = [UIBezierPath bezierPathWithRoundedRect:bounds byRoundingCorners:UIRectCornerAllCorners cornerRadii:cornerRadius];
+    } else if (indexPath.row == 0) {
+        path = [UIBezierPath bezierPathWithRoundedRect:bounds byRoundingCorners:UIRectCornerTopLeft | UIRectCornerTopRight cornerRadii:cornerRadius];
+    } else if (indexPath.row == [tableView numberOfRowsInSection:indexPath.section]-1) {
+        path = [UIBezierPath bezierPathWithRoundedRect:bounds byRoundingCorners:UIRectCornerBottomLeft | UIRectCornerBottomRight cornerRadii:cornerRadius];
+    }else {
+        path = [UIBezierPath bezierPathWithRect:bounds];
+    }
+    layer.path = path.CGPath;
+    layer.fillColor = fillColor.CGColor;
+    UIView *testView = [[UIView alloc] initWithFrame:bounds];
+    [testView.layer insertSublayer:layer atIndex:0];
+    testView.backgroundColor = UIColor.whiteColor;
+    cell.backgroundView = testView;
+}
+
 - (void)jz_clearsSelection{
     NSArray *selectedIndexPaths = [self indexPathsForSelectedRows];
     for (NSIndexPath *indexPath in selectedIndexPaths) {
